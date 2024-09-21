8e62825ab2ce8a13f7059bddc9c01a253acb42d2
programmershakib <programmershakibinfo@gmail.com>
2024-09-21 15:25:05 +0600

    final commit

--- js/todo.js
+++ js/todo.js
@@ -1,45 +1,27 @@
+let count = 0;
 document.getElementById('task-btn')
     .addEventListener('click', function(){
         const inputValue = document.getElementById('input-value').value;
+        count += 1;
+        const todoList = document.getElementById('content-container');
+        todoList.innerHTML += `
+            <div class="flex justify-between space-y-3">
+                <p>${count}</p>
+                <p>${inputValue}</p>
+                <button class="btn btn-xs delete-btn btn-warning">delete</button>
+            </div>
+        `
+        document.getElementById("input-value").value = "";
 
-        const contentContainer = document.getElementById('content-container');
-        const tr = document.createElement('tr');
-        const th = document.createElement('th');
-        th.innerText = 'list';
-        const td = document.createElement('td');
-        td.innerText = inputValue;
-        const button = document.createElement('button');
-        button.setAttribute('id', 'clear');
-        button.innerText = 'delete';
-        button.classList.add('btn-primary');
-        button.classList.add('rounded-lg');
-        button.classList.add('p-1');
-        button.classList.add('mt-2');
-        button.classList.add('list-delete');
+        const deleteButton = document.getElementsByClassName('delete-btn');
 
-        tr.appendChild(th);
-        tr.appendChild(td);
-        tr.appendChild(button);
-        contentContainer.appendChild(tr);
-        const clear = document.getElementById('clear')
-            .addEventListener('click', function(){
-                clear.classList.add('hidden');
-        });
+        for (const button of deleteButton) {
+            button.addEventListener("click", function (e) {
+              e.target.parentNode.style.display = "none";
+            });
+        }
 });
 
-// const listDelete = document.getElementsByClassName('list-delete').addEventListener('click', function(){
-//     listDelete.classList.add('hidden');
-// });
-
-// document.getElementById('clear').addEventListener('click', function(){
-//     console.log('click')
-//     // const allClear = document.getElementsByClassName('all-clear');
-//     // allClear.classList.add('hidden');
-//     // allClear.classList.remove('hidden');
-// });
-// document.getElementById('clear-btn').addEventListener('click', function(){
-//     console.log('click')
-//     const allClear = document.getElementById('content-container');
-//     // allClear.classList.add('hidden');
-//     allClear.classList.remove('hidden');
-// });
+document.getElementById("clear-btn").addEventListener("click", function () {
+    document.getElementById("content-container").style.display = "none";
+});
